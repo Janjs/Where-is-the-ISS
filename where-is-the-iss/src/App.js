3638622d5b7829d89d4ab288d9e77361aa3e16f1
Jan J. Serra <jan.jime.serra@gmail.com>
2021-06-11 11:23:49 +0200

    Add reverse geolocalization

--- where-is-the-iss/src/App.js
+++ where-is-the-iss/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import {
   ComposableMap,
   Geographies,
@@ -6,49 +6,87 @@ import {
   Marker,
 } from "react-simple-maps";
 import React, { useEffect, useState } from "react";
+import ReactTooltip from "react-tooltip";
+require("dotenv").config();
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-
 const App = () => {
   const [issLoc, setIssLoc] = useState({});
   const [isLoading, setLoading] = useState(true);
+  const [tooltipContent, setTooltipContent] = useState("");
+  const [formattedAddress, setFormattedAddress] = useState("");
 
   useEffect(() => {
     fetch("https://api.wheretheiss.at/v1/satellites/25544")
       .then((response) => response.json())
-      .then((json) => setIssLoc(json))
+      .then((json) => {
+        setIssLoc(json);
+        console.log(
+          "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+            json.latitude +
+            "," +
+            json.longitude +
+            "&key=" +
+            process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+        );
+        fetch(
+          "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+            json.latitude
+          + "," +
+            json.longitude 
+          + "&key=" +
+            process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            if(json.results.length == 0){
+              setFormattedAddress("prob in the ocean");
+            } else {
+              setFormattedAddress(json.results[0].formatted_address);
+            }
+          });
+      })
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
 
-  console.log(issLoc)
+  console.log(formattedAddress);
 
   return (
     <div style={style.container} className="App">
       <h1 style={style.title}>Here is where the ISS 🛰️ is flying over</h1>
       {!isLoading && (
         <div>
-          <ComposableMap>
-              <Geographies geography={geoUrl} style={style.map} width={500}>
-                {({ geographies }) =>
-                  geographies.map((geo) => (
-                    <Geography key={geo.rsmKey} geography={geo} />
-                  ))
-                }
-              </Geographies>
-              <Marker coordinates={[issLoc.longitude, issLoc.latitude]}>
-                <text textAnchor="middle" fontSize={16}>
-                  🛰️
-                </text>
-              </Marker>
+          <ComposableMap data-tip="">
+            <Geographies geography={geoUrl} style={style.map} width={500}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography key={geo.rsmKey} geography={geo} />
+                ))
+              }
+            </Geographies>
+            <Marker
+              coordinates={[issLoc.longitude, issLoc.latitude]}
+              onMouseEnter={() => {
+                setTooltipContent(formattedAddress);
+              }}
+              onMouseLeave={() => {
+                setTooltipContent("");
+              }}
+            >
+              <text textAnchor="middle" fontSize={16}>
+                🛰️
+              </text>
+            </Marker>
           </ComposableMap>
+          <ReactTooltip>{tooltipContent}</ReactTooltip>
         </div>
       )}
     </div>
   );
-}
+};
 
 const style = {
   container: {
@@ -60,7 +98,7 @@ const style = {
   },
   map: {
     fill: "#0b3d91",
-  }
+  },
 };
 
 export default App;
